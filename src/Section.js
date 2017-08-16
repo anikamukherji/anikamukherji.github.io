08e740a4cec9e0e8471706384bd98dd5408fd560
anika-mukherji <anika.mukherji@duke.edu>
2017-08-15 22:47:36 -0700

    removing unused images and files

--- src/Section.js
+++
@@ -1,8 +0,0 @@
-var React   = require('react');
-var Scroll  = require('react-scroll');
-
-var Link       = Scroll.Link;
-var Element    = Scroll.Element;
-var Events     = Scroll.Events;
-var scroll     = Scroll.animateScroll;
-var scrollSpy  = Scroll.scrollSpy;
